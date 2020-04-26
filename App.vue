972275bf89da4cecfdc8c5c1f1eba117de47457a
FabienEfrei <54938800+FabienEfrei@users.noreply.github.com>
2020-04-26 18:36:56 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>  
+    <h1>{{ title + i+ times}}</h1>
+    <button @click="iAmClicked()">Click if you can!</button>
+  </div>
+</template>
+ 
+<script>
+    export default {
+       
+        data() {
+            return {
+                title: 'you are a boss and you have cliked : ',
+                i : 0 ,
+                times :" times",
+            }
+        },
+        methods: {
+
+        
+            iAmClicked() {
+                this.i +=1;
+                this.title = "you are a boss and you have cliked : ";
+                this.times =" times";
+            },
+
+           
+        }
+    };
+</script>
